ac81008ddcbe7169c7726636bc35041d15d7d53c
ChengshengDeng <dylanden@bu.edu>
2023-03-27 22:19:16 +0800

    update

--- 文件操作/文件操作.py
+++ 文件操作/文件操作.py
@@ -6,7 +6,7 @@
 
 
 # open
-f = open("./测试.txt", mode='r', encoding="UTF-8")
+f = open("../测试.txt", mode='r', encoding="UTF-8")
 print(type(f))
 
 # 读取文件 - read()
@@ -22,21 +22,21 @@ line = f.readline()
 print(f'line对象的类型是{type(line)}, 数据内容是{line}')
 
 # for 循环读取文件
-for line in open("./测试.txt", mode='r', encoding="UTF-8"):
+for line in open("../测试.txt", mode='r', encoding="UTF-8"):
     print(f'每一行的数据是{line}')
 
 # close() 关闭文件
 f.close()
 
 # with open()
-with open("./测试.txt", 'r') as f:
+with open("../测试.txt", 'r') as f:
     for line in f:
         print(f'每一行数据是:{line},类型是{type(line)}')
 
 
 # 单词计数：
 ## 方法一
-with open("./word.txt", mode='r', encoding='utf-8') as f:
+with open("../word.txt", mode='r', encoding='utf-8') as f:
     line = f.readlines()
     count = 0
     for sentence in line:
@@ -49,19 +49,19 @@ with open("./word.txt", mode='r', encoding='utf-8') as f:
 
 
 ## 方法二
-f = open("./word.txt", mode='r', encoding='utf-8')
+f = open("../word.txt", mode='r', encoding='utf-8')
 content = f.read()
 print(content.count('itheima'))
 
 # 文件的写出
-f = open('./test.txt', 'w', encoding='utf-8')
+f = open('../test.txt', 'w', encoding='utf-8')
 f.write('Hello World')  # 将内容写到内存中
 f.flush()  # 将内容写到硬盘中
 f.close()   # 内置flush功能
 
 
 # 文件的追加
-f = open('./test.txt', mode='a', encoding='utf-8')
+f = open('../test.txt', mode='a', encoding='utf-8')
 f.write('黑马程序员，学python最佳选择')
 f.close()
 
