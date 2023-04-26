df7cf0d4da26b0cbf6d8dc9c453dc8564eff1523
ChengshengDeng <chengshengdeng0308@163.com>
2023-04-26 17:23:13 +0800

    变量类型注解_函数和方法

---
+++ 11_对象/_类型注解_函数和方法.py
@@ -0,0 +1,13 @@
+# -*- coding = utf-8 -*-  
+# @Time: 2023/4/26 17:20 
+# @Author: Dylan 
+# @File: _类型注解_函数和方法.py 
+# @software: PyCharm
+
+# 对形参进行类型注解
+def add(x: int, y: int):
+    return x + y
+
+# 对返回值进行类型注解
+def func(data: list) -> list:
+    return data
