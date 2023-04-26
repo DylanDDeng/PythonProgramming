97158a8b41c327086c5bd743e7a583df02c6b5ee
ChengshengDeng <chengshengdeng0308@163.com>
2023-04-26 17:35:54 +0800

    Union1

--- 11_对象/12_类型注解_Union联合类型.py
+++ 11_对象/12_类型注解_Union联合类型.py
@@ -1,7 +1,7 @@
 # -*- coding = utf-8 -*-  
 # @Time: 2023/4/26 17:28 
 # @Author: Dylan 
-# @File: 12_l类型注解_Union联合类型.py 
+# @File: 12_类型注解_Union联合类型.py
 # @software: PyCharm
 
 """
@@ -10,5 +10,5 @@
 from typing import Union
 my_lsit: list[Union[int, str]] = [1, 2, "itheima", 'itcast']
 
-def func(data: Union[int, str])->Union[int, str]:
+def func(data: Union[int, str]) -> Union[int, str]:
     pass
